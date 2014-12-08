88ffdf583552b4549ad79ff81d131cad50dbbe5d
Milan Brankovic <milanbrankovic@Milans-MacBook-Pro.local>
2014-12-08 11:31:09 +0100

    Initial commit

---
+++ config.rb
@@ -0,0 +1,19 @@
+# Require any additional plugins...
+require "compass-normalize"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/stylesheets"
+sass_dir = "sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/js"
+fonts_dir = "assets/fonts"
+
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# Enable relative paths to assets via compass helper functions. Options "true", "false"
+relative_assets = true
+
+# Disable debugging comments that display the original location of your selectors. Options "true", "false"
+line_comments = false
