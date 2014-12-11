ad0cf1d1a99f8efc4a88096d62b1234f77219adf
Milan Brankovic <milanbrankovic@Milans-MacBook-Pro.local>
2014-12-11 09:47:18 +0100

    Add basic structure

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,17 @@
 // Scripts
 //------------------------------------------------------------
 //
+
+
+(function () {
+    $(".nav-toggle").on('click', function(e) {
+        e.preventDefault();
+
+        $("body").toggleClass("toggle");
+    });
+
+    $(".main-nav a").on('click', function() {
+        $("a").removeClass("active");
+        $(this).addClass("active");
+    });
+})();
