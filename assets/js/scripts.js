f787004e6fad19aaee324b94e92695f70e5271ce
Milan Brankovic <milanbrankovic@Milans-MacBook-Pro.local>
2014-12-12 15:43:05 +0100

    Add basic structure

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,8 +7,12 @@
     $(".nav-toggle").on('click', function(e) {
         e.preventDefault();
 
-        $("body").toggleClass("toggle active");
+        $("body").toggleClass("toggle");
         return false;
+    });
+
+    $(".overlay").on('click touchmove', function(e) {
+        $("body").removeClass("toggle");
     });
 
     $(".main-nav a").on('click', function() {
