9f1e099701cb930055ba7a09daaaf66093189b61
Milan Brankovic <milanbrankovic@Milans-MacBook-Pro.local>
2014-12-15 22:58:51 +0100

    Add basic structure

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,9 +12,11 @@
         return false;
     });
 
+
     $(".overlay").on('click touchmove', function() {
         $('body').removeClass('toggle');
     });
+
 
     $(".main-nav a").on('click', function() {
         $('a').removeClass('active');
@@ -31,6 +33,7 @@
         }
     });
 
+
     var removeClass = true;
     $(".submit").on('click', function() {
         $(".search-form").addClass('push');
@@ -46,6 +49,34 @@
             $(".search-form").removeClass('push');
         }
         removeClass = true;
+    });
+
+
+    $('.login-form-inputs').on('focus', function(e){
+        $(this).parent().addClass('active');
+    });
+
+    $('.login-form-inputs').on('blur', function(e){
+        if (!$(this).val()){
+            $(this).parent().removeClass('active');
+        }
+    });
+
+    $('.login-form-inputs').each(function(i, item){
+        if ($(item).val()){
+            $(this).parent().addClass('active');
+        }
+    });
+
+
+    $(".tabs-links a").on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+
+        jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
+
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
     });
 
 })();
