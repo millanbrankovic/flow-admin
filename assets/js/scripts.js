ab097a062ac4b27aa6a1aa9f38cd083ad9d1dd12
Milan Brankovic <milanbrankovic@Milans-MacBook-Pro.local>
2014-12-17 16:08:57 +0100

    Add basic structure

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -52,18 +52,18 @@
     });
 
 
-    $('.login-form-inputs').on('focus', function(e){
+    $(".login-form-inputs").on('focus', function(e){
         $(this).parent().addClass('active');
     });
 
-    $('.login-form-inputs').on('blur', function(e){
+    $(".login-form-inputs").on('blur', function(e){
         if (!$(this).val()){
             $(this).parent().removeClass('active');
         }
     });
 
-    $('.login-form-inputs').each(function(i, item){
-        if ($(item).val()){
+    $(".login-form-inputs").each(function(i, item){
+        if($(item).val()){
             $(this).parent().addClass('active');
         }
     });
