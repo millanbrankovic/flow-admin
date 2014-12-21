1da940cbae9ac53432824bf40947995e1856bbe2
Milan Brankovic <milanbrankovic@Milans-MacBook-Pro.local>
2014-12-21 14:52:00 +0100

    Add basic structure

--- assets/js/vendor/datepicker.js
+++ assets/js/vendor/datepicker.js
@@ -233,8 +233,8 @@
 
         // internationalization
         i18n: {
-            previousMonth : 'Previous Month',
-            nextMonth     : 'Next Month',
+            previousMonth : '&laquo;',
+            nextMonth     : '&raquo;',
             months        : ['January','February','March','April','May','June','July','August','September','October','November','December'],
             weekdays      : ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
             weekdaysShort : ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
